d20b7df3ee50949bbd3af2960373a22d51dae7b5
Danilo Marchesani <danilo@Danilos-MacBook-Pro.local>
2023-10-03 17:28:38 +0200

    created form

---
+++ client/src/Success.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './success.css'
+
+function Success() {
+  return (
+    <div className='style-success'>
+        Success!!!
+    </div>
+  )
+}
+
+export default Success
