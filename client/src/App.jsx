7c4e43860711d3ecdbf192691c4efde11e8b4934
Danilo Marchesani <danilo@Danilos-MacBook-Pro.local>
2023-10-05 15:23:03 +0200

    ended frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import Success from './Success'
 import './App.css'
 
 function App() {
-  const [showSuccess, setShowSuccess] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [isEmpty, setIsEmpty] = useState('');
   
   const sendValue = (e) => {
     e.preventDefault();
@@ -16,16 +17,27 @@ function App() {
   
   }
 
+  const checkIfEmpty = (e) => {
+    const valore = e.target.value;
+    if(valore && valore.length > 5) {
+      setIsEmpty(valore);
+    } else {
+      setIsEmpty(false)
+    }
+    
+  }
+
   return (
     <div>
       <h1>Check Presenza</h1>
       <form onSubmit={sendValue}>
         <div className='form-style'>
           <label htmlFor="codice">Inserisci Codice Studente</label>
-          <input type="text" placeholder='Il tuo codice...'/>
+          <input type="text" placeholder='Il tuo codice...' id='inputCodice' name='codice' onChange={checkIfEmpty}/>
         </div>
-        <input type="submit" value="Entra" id="btnEntra"/>
+        <button type="submit" id="btnEntra" disabled={!isEmpty}>Invia</button>
       </form>
+      <p className='danger' hidden={isEmpty}>Il tuo codice deve essere di almeno 5 caratteri...</p>
 
     {
       showSuccess ? <Success /> : null
